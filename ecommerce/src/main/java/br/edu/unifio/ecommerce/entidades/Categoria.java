fa93e6f3b79ec63792f444c90768bcfca1165bc8
pavaninho <129853317+pavaninho@users.noreply.github.com>
2026-09-03 00:12:00 +0000

    fff

--- ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Categoria.java
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Categoria.java
@@ -1,16 +1,23 @@
 package br.edu.unifio.ecommerce.entidades;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 //import lombok.AllArgsConstructor;
 import lombok.Getter;
 //import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 //@NoArgsConstructorArg
 //@AllArgsConstructor
 
 public class Categoria {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Short id;
     private String nome;
     private String descriçao;
